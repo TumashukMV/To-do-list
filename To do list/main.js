8377ac41fa0f2ab3cdaa85c8f553701e21cc1151
TumashukMV <mikhail.tumashuk2@gmail.com>
2020-07-22 16:54:37 +0300

    module

--- To do list/main.js
+++ To do list/main.js
@@ -1,14 +1,6 @@
     
     var ul = document.querySelector("ul");
     var spans = document.getElementsByTagName("span");
-    /*var input = document.querySelector("input[type='text']");
-    var container = document.querySelector("div");
-    var lists = document.querySelectorAll("li");
-    var saveBtn = document.querySelector(".save");
-    var clearBtn = document.querySelector(".clear");
-    var tipsBtn = document.querySelector(".tipBtn");
-    var closeBtn = document.querySelector(".closebtn");
-    var overlay = document.getElementById("overlay");*/
 
     import { ulCall } from "./ul.js";
     import { callPencil } from "./pencil.js";
@@ -40,50 +32,5 @@
         }
     }
 
-    /*tipsBtn.addEventListener("click", function(){
-        overlay.style.height = "100%";
-    });
-    
-    closeBtn.addEventListener("click", function(event){
-        event.preventDefault();
-        overlay.style.height = "0";
-    });
-
-    pencil.addEventListener('click', function(){
-        input.classList.toggle("display");
-    })*/
-
-    /*ul.addEventListener("click", function(e){
-        if(e.target.tagName === 'LI'){
-        e.target.classList.toggle("checked");
-    }
-    input.addEventListener('keypress', function(key){
-        console.log(key);
-        if (key.which === 13){
-            var li = document.createElement('li');
-            var spanElement = document.createElement("span");
-            var icon = document.createElement("i");
-            
-            var newTodo = this.value;
-            this.value = "";
-
-            icon.classList.add('fas', 'fa-trash-alt');
-            spanElement.append(icon);
-            ul.appendChild(li).append(spanElement, newTodo);
-        }
-    });
-});
-
-//Add todo to
-
-
-//Clear all todos on click Clear button
-clearBtn.addEventListener('click', function(){
-    ul.innerHTML = "";
-});
-
-saveBtn.addEventListener('click', function(){
-    localStorage.setItem('todoList', ul.innerHTML);
-})*/
 deleteTodo();
 loadTodos();
