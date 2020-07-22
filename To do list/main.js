80cdadf2288f919ac7577459f86de2482ae7750f
TumashukMV <mikhail.tumashuk2@gmail.com>
2020-07-22 12:54:37 +0300

    cjg

--- To do list/main.js
+++ To do list/main.js
@@ -1,39 +1,3 @@
-/*document. addEventListener("DOMContentLoaded", function (event){
-    function calculate() {
-    var bill_count = document.getElementById("bill_count").value;
-    var service_quality = document.getElementById("service_quality"). value;
-    var people_num = document.getElementById("people_num").value;
-    // Simple validation
-    if (!bill_count ||!service_quality) {
-    alert("Please enter values!");
-    return;
-    }
-    // Check people input value
-    if (!people_num || people_num <= 1) {
-    people_num =1;
-    } else {
-    document.getElementById("each"). style. display = "block";
-    }
-    
-    // Calculate tips
-    var total = (bill_count * service_quality) / people_num;
-    
-    //round value
-    total - Math.round(total * 100) / 100;
-    total - total.toFixed(2);
-    
-    document.getElementById("totalTip"). style. display - "block";
-    document.getElementById("tip"). innerHTML - total;
-    }
-    // Hide tips element on load
-    document.getElementById("totalTip").style.display - "none";
-    document.getElementById("each"). style. display - "none";
-    
-    document.getElementById("calculate").onclick - function () {
-    calculate();
-    }
-    
-    })*/
     var input = document.querySelector("input[type='text']");
     var ul = document.querySelector("ul");
     var container = document.querySelector("div");
@@ -97,7 +61,3 @@ clearBtn.addEventListener('click', function(){
 saveBtn.addEventListener('click', function(){
     localStorage.setItem('todoList', ul.innerHTML);
 })
-
-deleteTodos();
-
-loadTodos();
